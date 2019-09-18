2d5e2b5ce4619a22808c48ec4ca56a511932a5e4
boggs <hello@boggs.xyz>
2019-09-18 13:21:53 +0000

    IND-259 - Fix flaky redis by binding to ipv4 only instead of ipv4 and ipv6
    https://github.com/antirez/redis/issues/3241#issuecomment-444861063
    https://github.com/zulip/zulip/issues/11079

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,11 @@ if [ "$1" == "docker-start" ]; then
   shift
 
   mysqld --initialize-insecure 2> /dev/null
-  service mysql start
+  service mysql restart
   mysql -e 'CREATE USER ubuntu@localhost; GRANT ALL PRIVILEGES ON *.* to ubuntu@localhost; FLUSH PRIVILEGES;'
 
-  service mongodb start
-  redis-server --daemonize yes
+  service mongodb restart
+  service redis-server restart
 fi
 
 exec "$@"
