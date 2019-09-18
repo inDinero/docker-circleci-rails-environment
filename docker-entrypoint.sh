abad5130f9b79df94ffc03d16189a7a3af4722a2
boggs <hello@boggs.xyz>
2019-09-18 08:18:15 +0000

    IND-259 - Styling

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$1" == "docker-start" ]; then
 
   mysqld --initialize-insecure 2> /dev/null
   service mysql start
-  mysql -e 'CREATE USER ubuntu@localhost; GRANT ALL PRIVILEGES ON *.* to ubuntu@localhost ;FLUSH PRIVILEGES;'
+  mysql -e 'CREATE USER ubuntu@localhost; GRANT ALL PRIVILEGES ON *.* to ubuntu@localhost; FLUSH PRIVILEGES;'
 
   service mongodb start
   redis-server --daemonize yes
